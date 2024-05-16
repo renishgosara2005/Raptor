38acf87a84b8404605cd8747fa68ffdd0f0e5559
Renish Gosara <renishgosara2005@gmail.com>
2024-05-16 16:25:40 +0530

    done

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-55px";  
+  }
+} 
+
+/*  Slicky Slider  */
+
+
+
+              
